2bde113ca1a9c04fb4a1ec2198752189451dcdf2
Andrey Krasnoff <andrey.krasnoff@xyleme.com>
2014-10-09 11:00:13 +0400

    code was little bit refactored

--- grails-app/controllers/sweethome/DeviceController.groovy
+++ grails-app/controllers/sweethome/DeviceController.groovy
@@ -35,22 +35,18 @@ class DeviceController {
         def device = Device.get(params.id)
 
         if(device != null){
-            Sensor sensor = sensorFactory.get(device.containerClass, device.name, device.addr)
-            if(sensor == null)
-                render status: 404, text: "Cannot find device with address \"${device.addr}\""
-            else
-                sensor.withSensor { ->
-                    if(sensor) {
-                        def val = SensorUtils.addCorrection( sensor.read(), device.coefficient, device.correction)
-                        def formatted = sensor.format(val)
+            device.withSensor { sensor ->
+                if(sensor) {
+                    def val = SensorUtils.addCorrection( sensor.read(), device.coefficient, device.correction)
+                    def formatted = sensor.format(val)
 
-                        def readings = [val: val, formatted: formatted]
+                    def readings = [val: val, formatted: formatted]
 
-                        render readings as JSON
-                    } else {
-                        render status: 500, text: "Cannot define appropriate sensor"
-                    }
+                    render readings as JSON
+                } else {
+                    render status: 404, text: "Cannot find device with address \"${device.addr}\""
                 }
+            }
         } else {
             render status: 404, text: "Cannot find device with id \"${params.id}\""
         }
